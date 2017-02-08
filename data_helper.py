425d644d004851a1c2cbbd1d9b247e15166b8fee
ascane <mandy00724@gmail.com>
2017-02-08 23:53:04 +0100

    Fill missing data.

--- data_helper.py
+++ data_helper.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+from pykalman import KalmanFilter
+from theano.sandbox.cuda.basic_ops import row
 
 def load_data(filepath='data/', split='train'):
 	"""load all data"""
@@ -50,3 +53,91 @@ def plot_data(x, y):
 
 
 # plot_data(x_train, y_train)
+
+x_new_input_train, _ = load_data(split='train')
+x_new_input_test = load_data(split='test')
+x_new_input = pd.concat([x_new_input_train, x_new_input_test])
+
+
+# bat_id = 1
+x_train_all, y_train_all = load_data(split='train')
+bat_id = 1
+x_train, y_train = get_bat(bat_id, x_train_all, y_train_all)
+x_test_all = load_data(split='test')
+x_test = get_bat(bat_id, x_test_all)
+
+x = pd.concat([x_train, x_test])
+x = x.ix[:,'x1':]
+x = np.ma.masked_invalid(x)
+
+kl = KalmanFilter(n_dim_obs=5, n_dim_state=5)
+model = kl.em(x, n_iter=2)
+x_smooth_1, _ = model.smooth(x)
+
+# bat_id = 2
+x_train_all, y_train_all = load_data(split='train')
+bat_id = 2
+x_train, y_train = get_bat(bat_id, x_train_all, y_train_all)
+x_test_all = load_data(split='test')
+x_test = get_bat(bat_id, x_test_all)
+
+x = pd.concat([x_train, x_test])
+x = x.ix[:,'x1':]
+x = np.ma.masked_invalid(x)
+
+kl = KalmanFilter(n_dim_obs=5, n_dim_state=5)
+model = kl.em(x, n_iter=2)
+x_smooth_2, _ = model.smooth(x)
+
+# bat_id = 3
+x_train_all, y_train_all = load_data(split='train')
+bat_id = 3
+x_train, y_train = get_bat(bat_id, x_train_all, y_train_all)
+x_test_all = load_data(split='test')
+x_test = get_bat(bat_id, x_test_all)
+
+x = pd.concat([x_train, x_test])
+x = x.ix[:,'x1':]
+x = np.ma.masked_invalid(x)
+
+kl = KalmanFilter(n_dim_obs=5, n_dim_state=5)
+model = kl.em(x, n_iter=2)
+x_smooth_3, _ = model.smooth(x)
+
+# bat_id = 4
+x_train_all, y_train_all = load_data(split='train')
+bat_id = 4
+x_train, y_train = get_bat(bat_id, x_train_all, y_train_all)
+x_test_all = load_data(split='test')
+x_test = get_bat(bat_id, x_test_all)
+
+x = pd.concat([x_train, x_test])
+x = x.ix[:,'x1':]
+x = np.ma.masked_invalid(x)
+
+kl = KalmanFilter(n_dim_obs=5, n_dim_state=5)
+model = kl.em(x, n_iter=2)
+x_smooth_4, _ = model.smooth(x)
+
+x_smooth = [x_smooth_1, x_smooth_2, x_smooth_3, x_smooth_4]
+
+incr = [0, 0, 0, 0]
+x_new_input_copy = x_new_input.copy()
+for index, row in x_new_input_copy.iterrows():
+	id_bat, time, x1, x2, x3, x4, x5 = row
+	if np.isnan(x1):
+		x_new_input.set_value(index, 'x1', x_smooth[id_bat - 1][incr[id_bat - 1], 0])
+	if np.isnan(x2):
+		x_new_input.set_value(index, 'x2', x_smooth[id_bat - 1][incr[id_bat - 1], 1])
+	if np.isnan(x3):
+		x_new_input.set_value(index, 'x3', x_smooth[id_bat - 1][incr[id_bat - 1], 2])
+	if np.isnan(x4):
+		x_new_input.set_value(index, 'x4', x_smooth[id_bat - 1][incr[id_bat - 1], 3])
+	if np.isnan(x5):
+		x_new_input.set_value(index, 'x5', x_smooth[id_bat - 1][incr[id_bat - 1], 4])
+	
+	incr[id_bat - 1] += 1
+
+print x_new_input
+	
+x_new_input.to_csv('data/input_new.csv', sep=';')
