ed7350b06f0267bb3c530e11e1d8e461ca0bb6f4
dex <dexiongc@gmail.com>
2017-02-25 15:14:16 +0100

    update

--- data_helper.py
+++ data_helper.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np 
 
 def load_data(filepath='data/', split='train'):
 	"""load all data"""
@@ -7,10 +8,12 @@ def load_data(filepath='data/', split='train'):
 		output_train_file = filepath + 'challenge_output_data_training_file_oze_energies_optimizing_energy_consumptions.csv'
 		input_train = pd.read_csv(input_train_file, delimiter=';', header=0, index_col=0, na_values=[''])
 		output_train = pd.read_csv(output_train_file, delimiter=';', header=0, index_col=0, na_values=[''])
+		input_train['Time'] = pd.to_datetime(input_train['Time'])
 		return input_train, output_train
 	else:
 		input_test_file = filepath + 'input_test.csv'
 		input_test = pd.read_csv(input_test_file, delimiter=';', header=0, index_col=0, na_values=[''])
+		input_test['Time'] = pd.to_datetime(input_test['Time'])
 		return input_test
 
 def split_data(x, y=None, cut_percent=0.7):
@@ -33,16 +36,50 @@ def get_bat(bat_id, x, y=None):
 def plot_data(x, y):
 	"""plot train data"""
 	import matplotlib.pyplot as plt
+	# import matplotlib.dates as mdates
 	fig, axes = plt.subplots(2, sharex=True)
 	x.plot(x='Time', y=['x1','x2', 'x3', 'x4', 'x5'], ax=axes[0])
-	y.plot(x=x_train['Time'], y=['y1','y2','y3','y4','y5'], ax=axes[1])
+	y.plot(x=x['Time'], y=y.columns.get_values(), ax=axes[1])
+	# fig.autofmt_xdate()
+	# plt.draw()
 	plt.show()
+
+def rnn_load_data(bat_id, timesteps, cut_percent=0.8):
+	u_train_all, y_train_all = load_data(filepath='data/complete/', split='train')
+	u_train_val, y_train_val = get_bat(bat_id, u_train_all, y_train_all)
+	u_train_val = u_train_val.ix[:,'x1':]
+	# print u_train_val.shape
+	# u_train_val = u_train_val.interpolate()
+	x_train = np.zeros((y_train_val.shape[0] - timesteps, timesteps, y_train_val.shape[1]))
+	y_train = np.zeros((y_train_val.shape[0] - timesteps, y_train_val.shape[1]))
+	u_train = np.zeros((y_train_val.shape[0] - timesteps, timesteps, u_train_val.shape[1]))
+	for i in range(y_train_val.shape[0] - timesteps):
+		x_train[i, :, :] = y_train_val.iloc[i:i+timesteps, :]
+		y_train[i, :] = y_train_val.iloc[i+timesteps, :]
+		u_train[i, :, :] = u_train_val.iloc[i+1:i+timesteps+1, :]
+
+	idx = int(u_train_val.shape[0]*cut_percent) - timesteps - 1
+	# split data
+	x_train, x_val = x_train[:idx], x_train[idx:]
+	y_train, y_val = y_train[:idx], y_train[idx:]
+	u_train, u_val = u_train[:idx], u_train[idx:]
+	return dict(x=x_train, u=u_train, y=y_train), dict(x=x_val, u=u_val, y=y_val)
+
+
+
+
 
 
 # x_train_all, y_train_all = load_data()
 
 # x_train, y_train = get_bat(1, x_train_all, y_train_all)
-
+# import datetime
+# print pd.to_datetime(x_train['Time'])
+# import matplotlib.pyplot as plt
+# x_train.plot(x='Time', y=['x1'])
+# plt.show()
+# print y_train.columns.get_values()
+# plot_data(x_train, y_train)
 # print y_train.ix[y_train.shape[0]-101:,:]
 
 # x_train, y_train, x_test, y_test = split_data(x_train, y_train, 0.7)
